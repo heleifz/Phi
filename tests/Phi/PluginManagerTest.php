7195e2ab05e8e64d3e6feb33cc4a490c3fa8f2ce
helei <heleifz@126.com>
2014-07-09 23:07:16 +0800

    add test for pluginmanager

--- tests/Phi/PluginManagerTest.php
+++ tests/Phi/PluginManagerTest.php
@@ -2,10 +2,49 @@
 
 namespace Phi;
 
+use \Mockery as m;
+
 class PluginManagerTest extends \PHPUnit_Framework_TestCase {
 
-	public function testRegister() {
+	private $appMock;
+	private $fsMock;
 
+	public function setUp() {
+		$this->appMock = m::mock('Phi\\Application');
+		$this->fsMock = m::mock('Phi\\FileSystem');
+	}
+
+	public function testRegister() {
+		$manager = new \Phi\PluginManager($this->appMock, $this->fsMock);
+		$this->appMock->shouldReceive('registerParser')->with('TestParser');
+		$manager->register('TestParser');
+		$this->appMock->shouldReceive('registerGenerator')->with('TestGenerator');
+		$manager->register('TestGenerator');
 	}	
+
+	public function testRegisterDirectories() {
+		$this->fsMock->shouldReceive('fileName')->passthru();
+		$this->fsMock->shouldReceive('includeOnce')->times(3);
+		$this->fsMock->shouldReceive('walk')->andReturn(
+			array(
+				array('absolute' => 'dir/SomeParser.php'),	
+				array('absolute' => 'dir/SomeGenerator.php'),	
+			)
+		);
+		$this->appMock->shouldReceive('registerParser')->with('SomeParser');
+		$this->appMock->shouldReceive('registerGenerator')->with('SomeGenerator');
+		$manager = new \Phi\PluginManager($this->appMock, $this->fsMock);
+		$manager->registerDirectory('somedir');
+	}
+
+	/**
+     * @expectedException \Exception
+     */
+	public function testIllegalPluginName() {
+		$manager = new \Phi\PluginManager($this->appMock, $this->fsMock);
+		$this->appMock->shouldReceive('registerParser');
+		$this->appMock->shouldReceive('registerGenerator');
+		$manager->register('TestParsera');
+	}
 	
 }
